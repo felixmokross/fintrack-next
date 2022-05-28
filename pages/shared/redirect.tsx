a8f1bf40cfc293102ed8dfc4a377ef733e196e51
Felix Mokross <felixmokross@hey.com>
2022-05-28 13:57:31 +0200

    Use useEffect instead of useLayoutEffect in Redirect component (the latter should not be used server-side)

--- pages/shared/redirect.tsx
+++ pages/shared/redirect.tsx
@@ -1,10 +1,10 @@
 import { useRouter } from "next/router";
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 export function withRedirect(url: string) {
   return function Redirect() {
     const { replace } = useRouter();
-    useLayoutEffect(() => {
+    useEffect(() => {
       replace(url);
     }, [replace]);
     return null;
