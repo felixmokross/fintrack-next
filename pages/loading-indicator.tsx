644bba9a94f24275337a28005b761531cc34b880
Felix Mokross <felixmokross@hey.com>
2022-06-05 20:42:13 +0200

    Only show loading indicator if route loading takes more than 300 ms

--- pages/loading-indicator.tsx
+++ pages/loading-indicator.tsx
@@ -3,27 +3,38 @@ import { useEffect, useState } from "react";
 
 export function LoadingIndicator() {
   const { events } = useRouter();
-  const [loading, setIsLoading] = useState(false);
+  const [loadingIndicatorVisible, setLoadingIndicatorVisible] = useState(false);
   useEffect(() => {
+    let timeout: number | undefined = undefined;
+    let routeLoading = false;
     events.on("routeChangeStart", handleRouteChangeStart);
     events.on("routeChangeComplete", handleRouteChangeComplete);
 
     return () => {
       events.off("routeChangeStart", handleRouteChangeStart);
       events.off("routeChangeComplete", handleRouteChangeComplete);
+
+      if (timeout !== undefined) window.clearTimeout(timeout);
     };
 
     function handleRouteChangeStart() {
-      setIsLoading(true);
+      routeLoading = true;
+
+      timeout = window.setTimeout(() => {
+        if (routeLoading) {
+          setLoadingIndicatorVisible(true);
+        }
+      }, 300);
     }
 
     function handleRouteChangeComplete() {
-      setIsLoading(false);
+      routeLoading = false;
+      setLoadingIndicatorVisible(false);
     }
   }, [events]);
   return (
     <div className="flex animate-pulse items-center text-sm font-medium text-gray-500 dark:text-gray-400">
-      {loading && <>Loading…</>}
+      {loadingIndicatorVisible && <>Loading…</>}
     </div>
   );
 }
