e4ab9a0a588c5e1a5833153a4df3b4cb5f30cc65
Felix Mokross <felixmokross@hey.com>
2022-06-07 20:04:04 +0200

    Extend call-updater script to retry API calls if they run into a timeout

--- .github/actions/call-updater/dist/index.js
+++ .github/actions/call-updater/dist/index.js
@@ -9152,27 +9152,49 @@ main().catch((e) => {
 async function main() {
   const token = await getToken();
 
-  await fetchWithToken("/api/admin/update-forex-rates", "PUT", token);
-  await fetchWithToken("/api/admin/update-stock-prices", "PUT", token);
-  await fetchWithToken("/api/admin/recalculate", "PUT", token);
+  await callApi("/api/admin/update-forex-rates", "PUT", token);
+  await callApi("/api/admin/update-stock-prices", "PUT", token);
+  await callApi("/api/admin/recalculate", "PUT", token);
+}
+
+async function callApi(url, method, token) {
+  console.log(`Calling ${url}…`);
+
+  const response = await fetchWithRetries(url, method, token);
+  await ensureFetchSuccess(response);
+}
+
+async function fetchWithRetries(url, method, token) {
+  const maxNumberOfTries = 3;
+  let response;
+  for (let i = 0; i < maxNumberOfTries; i++) {
+    if (i > 0) console.log(`Timeout, retrying (${i + 1}/${maxNumberOfTries})…`);
+
+    response = await fetchWithToken(url, method, token);
+
+    if (response.status !== 504) {
+      return response;
+    }
+  }
+
+  throw new Error(
+    `Request failed ${maxNumberOfTries} times with timeout (HTTP status code 504). Last response message: ${await response.text()}`
+  );
 }
 
 async function fetchWithToken(url, method, token) {
   const baseUrl = process.env.BASE_URL;
 
-  console.log(`calling ${url}…`);
-  const response = await fetch(`${baseUrl}${url}`, {
+  return await fetch(`${baseUrl}${url}`, {
     method,
     headers: { Authorization: `Bearer ${token}` },
   });
-
-  await ensureFetchSuccess(response);
 }
 
 async function getToken() {
   const tokenEndpoint = `${process.env.AUTH0_ISSUER_BASE_URL}/oauth/token`;
 
-  console.log("fetching token…");
+  console.log("Fetching token…");
   const response = await fetch(tokenEndpoint, {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
